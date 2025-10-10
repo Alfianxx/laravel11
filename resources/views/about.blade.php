0608cfec4fcd71742d612304c1888f5426866c08
Alfian <alfianhacker@gmail.com>
2025-10-10 16:42:30 +0800

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,9 @@
+<x-layout>
+
+  <x-slot:title>{{ $nama }}</x-slot:title>
+
+  <h3 class="text-xl">Ini adalah halaman About</h3>
+  <p>Name : {{ $nama }}</p>
+  <p>About : {{ $title }}</p>
+  
+</x-layout>
