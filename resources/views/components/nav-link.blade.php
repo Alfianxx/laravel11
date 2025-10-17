7d43961e4caed22c05cd7dff47775a082432b301
Alfian <alfianhacker@gmail.com>
2025-10-17 16:38:07 +0800

    sampai di 6. view data, looping

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,1 +1,2 @@
+@props(['aktif' => false])
 <a {{ $attributes }} aria-current="page" class=" {{ $aktif ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-white/5 hover:text-white' }}   rounded-md  px-3 py-2 text-sm font-medium " aria-current="{{ $aktif ? 'page' : false }}">{{ $slot }}</a>
--- routes/web.php
+++ routes/web.php
@@ -10,8 +10,19 @@ Route::get('about', function () {
     return view('about', ['nama' => 'Alfian', 'title' => 'About Page']);
 });
 
-Route::get('blog', function () {
-    return view('blog', ['title' => 'Blog Page']);
+Route::get('posts', function () {
+    return view('posts', ['title' => 'Blog Page', 'posts' => [
+        [
+            'title' => 'Judul Artikel 1',
+            'author' => 'Alfian',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod perspiciiatis nihil, eligendi delectus ut doloremque excepturi ad numquam quae odit nisi neque molestiae ipsum assumenda, explicabo sit maiores facere voluptatum?',
+        ],
+        [
+            'title' => 'Judul Artikel 2',
+            'author' => 'Alfian',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, consectetur nisi debitis quasi rerum numquam necessitatibus! Recusandae libero enim natus? Voluptates officia officiis dolorem quasi, placeat voluptatum unde numquam rerum.',
+        ]
+    ]]);
 });
 
 Route::get('contact', function () {
