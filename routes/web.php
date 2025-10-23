33c60d2ddc9ec1d33488c0e0f30ff132ca3633d5
Alfian <alfianhacker@gmail.com>
2025-10-23 21:59:05 +0800

    membuat fungsi post all (eps 7. model)

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,29 @@
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
+class Post
+{
+    public static function all()
+    {
+        return [
+            [
+                'id' => 1,
+                'slug' => 'judul-artikel-1',
+                'title' => 'Judul Artikel 1',
+                'author' => 'Alfian',
+                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod perspiciiatis nihil, eligendi delectus ut doloremque excepturi ad numquam quae odit nisi neque molestiae ipsum assumenda, explicabo sit maiores facere voluptatum?',
+            ],
+            [
+                'id' => 2,
+                'slug' => 'judul-artikel-2',
+                'title' => 'Judul Artikel 2',
+                'author' => 'Alfian',
+                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, consectetur nisi debitis quasi rerum numquam necessitatibus! Recusandae libero enim natus? Voluptates officia officiis dolorem quasi, placeat voluptatum unde numquam rerum.',
+            ]
+        ];
+    }
+}
+
 Route::get('/', function () {
     return view('home', ['title' => 'Home Page']);
 });
@@ -12,52 +35,18 @@ Route::get('about', function () {
 });
 
 Route::get('posts', function () {
-    return view('posts', ['title' => 'Blog Page', 'posts' => [
-        [
-            'id' => 1,
-            'slug' => 'judul-artikel-1',
-            'title' => 'Judul Artikel 1',
-            'author' => 'Alfian',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod perspiciiatis nihil, eligendi delectus ut doloremque excepturi ad numquam quae odit nisi neque molestiae ipsum assumenda, explicabo sit maiores facere voluptatum?',
-        ],
-        [
-            'id' => 2,
-            'slug' => 'judul-artikel-2',
-            'title' => 'Judul Artikel 2',
-            'author' => 'Alfian',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, consectetur nisi debitis quasi rerum numquam necessitatibus! Recusandae libero enim natus? Voluptates officia officiis dolorem quasi, placeat voluptatum unde numquam rerum.',
-        ]
-    ]]);
+    return view('posts', ['title' => 'Blog Page', 'posts' => Post::all()]);
 });
 
 Route::get('/posts/{slug}', function ($slug) {
-    $posts =  [
-        [
-            'id' => 1,
-            'slug' => 'judul-artikel-1',
-            'title' => 'Judul Artikel 1',
-            'author' => 'Alfian',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod perspiciiatis nihil, eligendi delectus ut doloremque excepturi ad numquam quae odit nisi neque molestiae ipsum assumenda, explicabo sit maiores facere voluptatum?',
-        ],
-        [
-            'id' => 2,
-            'slug' => 'judul-artikel-2',
-            'title' => 'Judul Artikel 2',
-            'author' => 'Alfian',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, consectetur nisi debitis quasi rerum numquam necessitatibus! Recusandae libero enim natus? Voluptates officia officiis dolorem quasi, placeat voluptatum unde numquam rerum.',
-        ]
-        ];
 
-        $post = Arr::first($posts, function ($posts) use ($slug) {
-            return $posts['slug'] == $slug;
-        });
-        
+    $post = Arr::first(Post::all(), function ($posts) use ($slug) {
+        return $posts['slug'] == $slug;
+    });
+
     return view('post', ['title' => 'Single Post', 'post' => $post]);
-
 });
 
 Route::get('contact', function () {
     return view('contact', ['title' => 'Kontak']);
 });
-
-
